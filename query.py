a4cb9b84064cef0c154d61fb98c6fbee197c5113
Stephen-Wu-TVBS <stephenwu@tvbs.com.tw>
2025-06-10 17:48:05 +0800

    Update query.py add time calculate

--- query.py
+++ query.py
@@ -1,16 +1,19 @@
+# query.py
+# 載入環境變數，# .env 檔案中應包含 OPENAI_API_KEY
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
-# query.py
-# 載入環境變數，# .env 檔案中應包含 OPENAI_API_KEY
+import time
 from dotenv import load_dotenv
 load_dotenv() 
+
+
 # 載入向量資料庫
 embedding = OpenAIEmbeddings()
 # 使用 Chroma 作為向量資料庫
 db = Chroma(persist_directory="./mydb", embedding_function=embedding)
-# 構建檢索器
+# 構建檢索器 拿最接近的三段
 retriever = db.as_retriever(search_kwargs={"k": 3})
 # 查詢向量資料庫
 llm = OpenAI(temperature=0)
@@ -19,12 +22,14 @@ qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
 # 查詢示例
 query = "請問抗三高的原則？"
 # 執行查詢
+start = time.time()
 result = qa.run(query)
-# 輸出檢索到的段落和查詢結果
+print(f"⏱️ 回答用時：{round(time.time() - start, 2)} 秒")
 relevant_docs = retriever.get_relevant_documents(query)
-print("\n📄 檢索到的段落：")
-for i, doc in enumerate(relevant_docs, 1):
-    print(f"\n--- Chunk {i} ---\n{doc.page_content}\n")
-     
+# 輸出檢索到的段落和查詢結果
+# print("\n📄 檢索到的段落：")
+# for i, doc in enumerate(relevant_docs, 1):
+#     print(f"\n--- Chunk {i} ---\n{doc.page_content}\n")
+
 print("\n🔍 查詢結果：")
 print(result)
